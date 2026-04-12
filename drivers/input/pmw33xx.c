f3d7b095f76186136eb03e0875cccf4698e55108
Alexey Shchelokov <alexey.shchelokov@gmail.com>
2026-04-11 21:15:24 -0400

    fix preprocessor definitions

--- drivers/input/pmw33xx.c
+++ drivers/input/pmw33xx.c
@@ -15,10 +15,12 @@
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/sys/__assert.h>
 #include <zephyr/logging/log.h>
-#ifdef CONFIG_PMW33XX_3360
+#ifdef PMW33XX_3360
 #include "pmw3360_srom.h"
-#else
+#elif PMW33XX_3389
 #include "pmw3389_srom.h"
+#else
+#error "Please define PMW33XX_3360 or PMW33XX_3389
 #endif
 
 #include "pmw33xx.h"
